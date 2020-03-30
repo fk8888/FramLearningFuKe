6cb67bb18cfefa096c02b87d04b280960875f83f
赵栋亮 <zhaodongliang@itimebox.com>
2020-03-30 22:41:14 +0800

    上传分支

--- app/src/main/java/com/fk/framlearningfuke/MainActivity.java
+++ app/src/main/java/com/fk/framlearningfuke/MainActivity.java
@@ -1,9 +1,14 @@
 package com.fk.framlearningfuke;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author NineTailDemonFox
+ * @date or 2020/3/30 22:39
+ * 功能描述： 这个是主页面
+ * 联系方式：1037438704@qq.com
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
